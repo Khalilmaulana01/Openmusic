10817a09f34a19ff1599cc812bd5274093144696
prometheus <khalilalkhabasan123@gmail.com>
2021-08-13 02:44:35 +0700

    perbaiki constraint

--- migrations/1628710840839_create-table-playlistsongs.js
+++ migrations/1628710840839_create-table-playlistsongs.js
@@ -18,19 +18,19 @@ exports.up = (pgm) => {
     },
   });
 
-  pgm.addContraints(
+  pgm.addConstraint(
     'playlistsongs',
     'unique_playlist_id_and_song_id',
     'UNIQUE(playlist_id, song_id)',
   );
 
-  pgm.addContraints(
+  pgm.addConstraint(
     'playlistsongs',
     'fk_playlistsongs.playlist_id_playlists.id',
     'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCASE',
   );
 
-  pgm.addContraints(
+  pgm.addConstraint(
     'playlistsongs',
     'fk_playlistsongs.song_id_songs.id',
     'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE',
